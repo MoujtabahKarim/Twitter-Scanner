2c2e8a83f53fbe781576cc6302f6afff0341b694
Randy Taylor <tehgecKozzz@gmail.com>
2017-10-31 00:22:27 -0400

    Pythonify the code

--- main.py
+++ main.py
@@ -1,35 +1,24 @@
-import bs4
-
-from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen as u_req
 
-twitterName = input("Enter the twitter userName: ")
-url = "https://twitter.com/" + twitterName
+twitter_name = input("Enter the twitter username: ")
+url          = "https://twitter.com/" + twitter_name
 
-#Opens the connection and grabs the page
-uClient = uReq(url)
-page_html = uClient.read()
-uClient.close()
+# Opens the connection and grabs the page
+u_client  = u_req(url)
+page_html = u_client.read()
+u_client.close()
 
+# Converts the page to html (parsing)
+page_soup  = soup(page_html, "html.parser")
+containers = page_soup.find_all("div", {"class": "content"})
 
-#Converts the page to html (parsing)
-page_soup = soup(page_html, "html.parser")
+with open("Twitter-Info.csv", "w") as f:
+    headers = twitter_name + ", Their Tweet\n"
+    f.write(headers)
 
-containers = page_soup.findAll("div", {"class": "content"})
-filename = "Twitter-Info.csv"
-f = open(filename, "w")
-headers = twitterName + ", Their Tweet\n"
-
-f.write(headers)
-counter = 0
-for currentTweet in containers:
-
-    tweet = containers[counter].p.text
-    f.write( str(counter) + "," + tweet.replace(",", ";") + "\n")
-    print("----------")
-    print(counter + 1, ": " , tweet)
-    counter = counter + 1
-
-
-
-f.close()
+    for counter, current_tweet in enumerate(containers):
+        tweet = containers[counter].p.text
+        f.write( str(counter) + "," + tweet.replace(",", ";") + "\n")
+        print("----------")
+        print(counter + 1, ": " , tweet)
